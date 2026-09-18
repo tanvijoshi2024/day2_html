c6fe8ccdcfd84bfc61b882ad2bde8d229189d726
tanvijoshi2024 <tanvijoshi2024@gmail.com>
2026-09-18 14:21:49 +0530

    add this file

--- lesson2/lesson3/l1.py
+++ lesson2/lesson3/l1.py
@@ -1,46 +1,63 @@
 <style>
-    .subscribe-button
-    {
-        background-color:red;
-        color : rgb(255,255,255);
-        border:none;
-        height:36px;
-        width:105px;
-        border-radius:20px;
-        cursor:pointer;
-        margin-right:8px;
-        padding:200px;
-    }
-    .subscribe-button:hover{
-    background-color:green;
-}
-    .join-button
-    {
-        background-color:white;
-        border-color:rgb(41,56,78);
-        border-style:solid;
-        border-width:1px;
-        color:rgb(41,118,211);
-        height:36px;
-        width:62px;
-        border-radius:2px;
-        cursor:pointer;
-    }
-    .tweet-button
-    {
-       background-color:blue; 
-       color:rgb(255,255,255);
-       border:none;
-       height:36px;
-       width:74px;
-       border-radius:18px;
-       font-weight: bold;
-       font-size:15px;
-       cursor:pointer;
-       margin-left:8px;
+ .subscribe-button{
+ 
+     background-color:rgb(200,0,0);
+     color:white;
+     border:none;
+     height:36;
+     width: 105px;
+     border-radius:2px;
+     cursor:pointer;
+     margin-right:8px;
+     transition:opacity 1s;
+ }
+ .subscribe-button:hover{
+  
+       background-color:green;
+        opacity:0.6;
+ }
+ .subscribe-button:active{
+     background-color:blue;
+     opacity:0.4;
+ }
+ .join-button{
+    background-color:white;
+    border-color:rgb(41,118,211);
+    border-style:solid;
+    border-width:1px;
+    color:rgb(41,118,211);
+    height:36px;
+    width:62px;
+    border-radius:2px;
+    cursor:pointer;
+    transition:background-color 1s, color 1s;
+
+ }
+  .join-button:hover{
+     background-color:rgb(41,118,211);
+     color:white;
 
     }
+ .join-button:active{
+     opacity:0.7;
+
+    }
+ .tweet-button{
+     background-color:rgb(2,158,255);
+     color:white;
+     border:none;
+     height:36px;
+     width:74px;
+     border-radius:18px;
+     font-weight:bold;
+     font-size:15px;
+     cursor:pointer;
+     margin-left:8px;
+     box-shadow:0 0 0 black;
+
+     }
 </style>
-<button class="subscriber-button">SUBSCRIBE</button>
+
+<button class="subscribe-button">SUBSCRIBE</button>
 <button class="join-button">JOIN</button>
 <button class="tweet-button">TWEET</button>
